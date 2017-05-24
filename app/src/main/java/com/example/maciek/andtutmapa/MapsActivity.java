e04d943f41e0aa47fb29462ea192d12c932c719d
Maciek Wojnarowski <maciek@pizza.pl>
2017-05-25 00:33:52 +0200

    idk lol

--- app/src/main/java/com/example/maciek/andtutmapa/MapsActivity.java
+++ app/src/main/java/com/example/maciek/andtutmapa/MapsActivity.java
@@ -50,7 +50,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -191,7 +191,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     // Permission denied, Disable the functionality that depends on this permission.
                     Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
                 }
-                return;
+
             }
 
             // other 'case' lines to check for other permissions this app might request.
